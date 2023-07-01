e5b58c17abbb84060cc2e069d7d8f814965b21a3
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2023-07-02 00:29:19 +0700

    constructor class node

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -6,6 +6,14 @@ class Node
 {
 public:
 	string info;
-	Node* lefchild;
+	Node* leftchild;
 	Node* rightchild;
+
+	// Constructor for the node class
+	Node(string i, Node* l, Node* r)
+	{
+		info = i;
+		leftchild = l;
+		rightchild = r;
+	}
 };
